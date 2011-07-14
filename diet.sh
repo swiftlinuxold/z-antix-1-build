4ab2a0f9f556550b2ef130c5e538dfd4e37cf2b1
Jason Hsu <github@jasonhsu.com>
2011-07-13 21:59:21 -0500

    2011-07-13 21:58

---
+++ diet.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Proper header for a Bash script.
+
+# Check for root user login
+if [ ! $( id -u ) -eq 0 ]; then
+	echo "You must be root to run this script."
+	echo "Please enter su before running this script again."
+	exit
+fi
+
+# This is the script for transforming antiX Linux into Diet Swift Linux.
+
+sh ../apt/main.sh
