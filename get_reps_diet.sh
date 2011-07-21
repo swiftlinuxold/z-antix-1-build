99f94050731667f57e61162e7c69da1e65dc0aaa
Jason Hsu <github@jasonhsu.com>
2011-07-21 13:26:06 -0500

    ssh-agent implemented in get_reps_diet.sh so that you only have to enter the password once.

--- get_reps_diet.sh
+++ get_reps_diet.sh
@@ -11,8 +11,6 @@ USERNAME=$(logname)
 
 # This is the script for obtaining all of the repositories needed to generate Diet Swift Linux.
 
-# Need to save password http://help.github.com/ssh-key-passphrases/
-
 get_rep ()
 	{
 	if [ -d "/home/$USERNAME/develop/$1" ]; then
@@ -24,6 +22,39 @@ get_rep ()
 	return 0
 	}
 
+# From http://info.solomonson.com/content/setting-ssh-agent-ask-passphrase-only-once
+# Start up ssh-agent to save your password.
+# This allows you to download all repositories while entering your password just once.
+
+tempfile=/tmp/ssh-agent.test
+ 
+#Check for an existing ssh-agent
+if [ -e $tempfile ]
+then
+    echo "Examining old ssh-agent"
+    . $tempfile
+fi
+ 
+#See if the agent is still working
+ssh-add -l > /dev/null
+ 
+#If it's not working yet, just start a new one.
+if [ $? != 0 ]
+then
+    echo "Old ssh-agent is dead..creating new agent."
+ 
+    #Create a new ssh-agent if needed
+    ssh-agent -s > $tempfile
+    . $tempfile
+ 
+    #Add the key
+    ssh-add
+fi    
+ 
+#Show the user which keys are being used.
+ssh-add -l
+
+# ssh-agent is now implemented.  Now it's time to download all repositories.
 
 get_rep 0-intro
 get_rep add_help
@@ -40,3 +71,6 @@ get_rep security
 get_rep slim
 get_rep sylpheed
 get_rep wallpaper-standard
+
+# Now that the repositories are downloaded, it's time to delete the saved passwords.
+rm -r /tmp/ssh*
